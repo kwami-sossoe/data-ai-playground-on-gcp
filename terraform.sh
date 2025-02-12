a7f9ab71b0d44a036fa076f01a8985a8272a0a48
Kwami S. Sossoe <103736827+ksossoe@users.noreply.github.com>
2025-02-12 20:11:05 +0100

    add cloudbuild for full destroy

--- terraform.sh
+++ terraform.sh
@@ -3,7 +3,7 @@ env=play
 # init 
 terraform -chdir=bootstrap fmt
 terraform -chdir=environments/${env} fmt
-terraform -chdir=bootstrap init -backend-config=../environments/${env}/tf-backend-${env}.conf -backend-config="prefix=$env-cni"
+terraform -chdir=bootstrap init -backend-config=../environments/${env}/tf-backend-${env}.conf
 
 # tf plan
 terraform -chdir=bootstrap plan -var-file=../environments/${env}/terraform-${env}.tfvars  -out=../../${env}-planfile -input=false -compact-warnings
